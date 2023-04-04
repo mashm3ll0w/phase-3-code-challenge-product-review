ad1c3ea1880d4260d05449f65a6243589c93817e
Charles Swaleh <charlswall@live.com>
2023-04-04 20:43:44 +0300

    Completed the leave_review instance method for the Product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  def leave_review(user, star_rating, comment)
+    if user.is_a?(User) && star_rating.is_a?(Integer) && comment.is_a?(String)
+      Review.create(
+        star_rating: star_rating,
+        comment: comment,
+        user_id: user.id,
+        product_id: self.id
+      )
+    else
+      "Please check the data types of your user/star_rating and/or comment"
+    end
+  end
 end
