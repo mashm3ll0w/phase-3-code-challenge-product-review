9262c73824207ac87d460ccaab45910c4cb9058b
Charles Swaleh <charlswall@live.com>
2023-04-05 21:17:25 +0300

    Created the favorite_products instance method for the User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :products, through: :reviews
+
+  def favorite_product
+    max_review_value = self.reviews.map { |review| review.star_rating }.max
+    self.reviews.select { |review| review.star_rating == max_review_value }[
+      0
+    ].product
+  end
 end
