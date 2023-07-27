79a9d77734fba0a4553749f9441c1495c91a25bb
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-07-27 11:07:49 +0300

    Updates to my active records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 require 'httparty'
 require 'json'
+require 'sinatra/activerecord'
+
+require_relative '../app/controllers/application_controller'
+require_relative '../app/models/recipe'
+require_relative '../app/models/ingredient'
+require_relative '../app/models/instruction.rb'
+require_relative '../app/models/video'
 
 puts "🌱 Seeding spices..."
 puts "Clearing existing seed..."
@@ -10,23 +17,26 @@ meals.each do |meal|
   response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=#{meal}")
   data = JSON.parse(response.body)
   puts "Seeding #{meal}...."
-  puts "API response: #{data.inspect}"
+  puts "API response for #{meal}: #{data.inspect}"
 
   begin
-  if data["meals"].nil?
+  meals_data = data["meals"]
+  if meals_data.nil?
     puts "No seeds.... Skipping..."
     next
   end
 
-  data["meals"].each do |innermeal|
+  meals_data.each do |innermeal|
     puts "Inner seeding +++++++++++++++++++++++++++++++++++++#{innermeal}..."
     ingredients = []
-    (1..20).each do |i|
-      ingredient_name = innermeal["strIngredient#{i}"]
-      ingredient_measure = innermeal["strMeasure#{i}"]
-      break if ingredient_name.nil? || ingredient_name.strip.empty?
-      ingredients << "#{ingredient_measure} #{ingredient_name}"
-    end
+    measures = []
+    (1..20).each do |n|
+      ingredient = innermeal["strIngredient#{n}"]
+      measure = innermeal["strMeasure#{n}"]
+      break if ingredient.nil? || ingredient.empty? || measure.nil? || measure.empty?
+      ingredients << ingredient
+      measures << measure
+      end
     
 
     recipe = Recipe.create(
@@ -34,8 +44,9 @@ meals.each do |meal|
       name: innermeal['strMeal'],
       ingredients: ingredients,
       instructions: innermeal['strInstructions'],
-      str_meal_thumb: innermeal['strMealThumb']
+      image_url: innermeal['strMealThumb']
     )
+    puts "Recipe of ID #{recipe.id} created"
     end
   rescue StandardError => e
     puts "Error while seeding #{meal}: #{e.message}"
@@ -43,52 +54,4 @@ meals.each do |meal|
 end
 
 
-
-# def extract_ingredients(meal)
-#   ingredients = []
-#   (1..20).each do |i|
-#     ingredient = meal["strIngredient#{i}"]
-#     measure = meal["strMeasure#{i}"]
-#     break if ingredient.nil? || ingredient.strip.empty?
-#     ingredients << "#{measure} #{ingredient}"
-#   end
-#   ingredients.join(',')
-# end
-
-
-# puts "Fetching ingredients..."
-# ingredient = 'chickenbreast'
-# response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=#{ingredient}")
-# data = JSON.parse(response.body)
-
-#   # Process the API data and save it to your database
-#   puts "Saving results..."
-#   data['meals'].each do |meal|
-#   recipe = Recipe.find_or_initialize_by(id_meal: meal['idMeal'])
-#   recipe.assign_attributes(
-#     name: meal['strMeal'],
-#     ingredients: extract_ingredients(meal),
-#     instructions: meal['strInstructions'],
-#     str_meal_thumb: meal['strMealThumb']
-#   )
-#   recipe.save
-#     recipe
-#   end
-
 puts "✅ Done seeding!"
-
-
-
-# Seed your database here
-# Recipe.create(
-#     name: "Matar Paneer",
-#     ingredients: "Matar, Paneer, Spices, Tomato, Onion",
-#     instructions: "1. Heat oil in a pan..."
-#   )
-  
-#   Video.create(
-#     title: "Matar Paneer Recipe",
-#     url: "https://www.youtube.com/watch?v=your-video-url",
-#     description: "Learn how to make delicious Matar Paneer...",
-#     recipe_id: 1 # The ID of the associated recipe
-#   )
